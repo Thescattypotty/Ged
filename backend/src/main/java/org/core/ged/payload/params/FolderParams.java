88da6f5293900624b2877ca3e4d50bd65cd02187
Thescattypotty <bnsyahya123@gmail.com>
2025-07-29 20:04:13 +0100

    feat: Ajouter la gestion des paramètres de requête pour les dossiers avec UUID pour parentsId

--- backend/src/main/java/org/core/ged/payload/params/FolderParams.java
+++ backend/src/main/java/org/core/ged/payload/params/FolderParams.java
@@ -1,10 +1,15 @@
 package org.core.ged.payload.params;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 public record FolderParams(
+    @RequestParam(name = "query", required = false)
     String query,
-    List<String> parentsId
+    @RequestParam(name = "parentsId", required = false)
+    List<UUID> parentsId
 ) {
     
 }
